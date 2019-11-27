97b5e565dcaf6dd26bd02f2a384ffef97fe128c4
Fernanda <fernanda.marroquin07@gmail.com>
2019-11-26 19:27:03 -0500

    add tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import App from '../App';
+import Login from '../components/Login';
 import ReactDOM from 'react-dom';
-import { create } from 'react-test-renderer'
+import { create } from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
 
+configure({adapter: new Adapter()});
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<App />, div);
 	ReactDOM.unmountComponentAtNode(div);
 });
 
-it('handles validation', () => {
-	expect(formIsValid).toBe(false);
-	instance.handleValidation();
-	expect(formIsValid)toBe(true);
-	expect(tree.toJSON()).toMatchSnapshot()
-
-})
+describe('Test submit form', () => {
+  it('Tests click event', () => {
+	const component = shallow(<Login />);
+	expect(component.contains(<div>Forgot my password</div>)).toBe(false);  
+  });
+});
